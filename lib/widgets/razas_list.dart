6ea84fafedf36fdb08588c0dc52a1741ad152819
David Cabrero Souto <david.cabrero@madsgroup.org>
2020-11-08 19:02:54 +0100

    Reescrita la lista de razas usando 'FutureBuilder'

--- lib/widgets/razas_list.dart
+++ lib/widgets/razas_list.dart
@@ -8,34 +8,58 @@ class RazasList extends StatefulWidget {
 }
 
 class _RazasListState extends State<RazasList> {
-  List<String> _breeds = [];
+  Future<List<String>> _breeds;
 
   @override
   void initState() {
     super.initState();
-    _loadData();
+    _breeds = _loadData();
   }
 
   @override
   Widget build(BuildContext context) {
     final TextStyle fontStyle = TextStyle(fontSize: Theme.of(context).textTheme.headline5.fontSize);
-    return ListView.builder(
-      itemCount: _breeds.length,
-      itemBuilder: (BuildContext context, int i) =>
-        ListTile(
-          title: Text(_breeds[i], style: fontStyle),
-          selected: false,
-        ),
-      );
-    }
+    
+    return FutureBuilder(
+      future: _breeds,
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done && !snapshot.hasError) {
+          List<String> data = snapshot.data;
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int i) =>
+            ListTile(
+              title: Text(data[i], style: fontStyle),
+              selected: false,
+            ),
+          );
+        }
+        else if (snapshot.connectionState != ConnectionState.done) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        else {
+          return Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('images/snoopy-penalty-box.gif'),
+                  Text('There was a error reading file'),
+                ],
+              ),
+            ),
+          );
+        }
+      }
+    );
+  }
   
-  void _loadData() async {
+  Future<List<String>> _loadData() async {
     AssetBundle asset = DefaultAssetBundle.of(context);
     String json = await asset.loadString('data/breeds_list.json');
     Map data = jsonDecode(json);
-    setState(() {
-        _breeds = data['message'].keys.toList();
-    });
+    return data['message'].keys.toList();
   }
 }
 
